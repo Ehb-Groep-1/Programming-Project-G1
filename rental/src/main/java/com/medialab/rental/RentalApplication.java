a9200480578c86e5d68d4a42acffb55b99663c0d
IbrahimBousba <hdydanb97@gmail.com>
2024-05-17 10:52:21 +0200

    added api to login

--- rental/src/main/java/com/medialab/rental/RentalApplication.java
+++ rental/src/main/java/com/medialab/rental/RentalApplication.java
@@ -3,7 +3,12 @@ package com.medialab.rental;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @SpringBootApplication
 @RestController
@@ -13,8 +18,16 @@ public class RentalApplication {
         SpringApplication.run(RentalApplication.class, args);
     }
 
-    @GetMapping
-    public String indexApi() {
-        return "Application is running";
+    @GetMapping("/api/hello")
+    public Map<String, String> indexApi() {
+        HashMap<String, String> response = new HashMap<>();
+        response.put("message", "Application is running");
+        return response;
+    }
+
+    @PostMapping("/api/login")
+    public Map<String, String> loginUser(@RequestBody Map<String, String> response) {
+        System.out.println(response);
+        return response;
     }
 }
--- rental/src/main/resources/templates/Public/JAVASCRIPT/logIn.js
+++ rental/src/main/resources/templates/Public/JAVASCRIPT/logIn.js
@@ -1,13 +1,48 @@
-function login() {
-    // Get input values
+document.addEventListener('DOMContentLoaded', () => {
+const loginForm = document.getElementById("login-form");
+const login = async (e) => {
+    e.preventDefault();
+    // console.log("logged in");
     const username = document.getElementById("username").value;
     const password = document.getElementById("password").value;
-
-    // You can add your login logic here (e.g., validate credentials, make API calls, etc.)
-    // For demonstration purposes, let's just display an alert message:
-    if (username && password) {
-        alert("Login successful!");
-    } else {
-        alert("Please enter valid credentials.");
-    }
+    // const response = await fetch("http://localhost:8080/api/hello")
+    // .then(result => {
+    //     if(!result.ok) {
+    //         throw data.status;
+    //     }
+    //     return result.json();
+    // })
+    // .catch(err => console.error(err));
+    // console.log(response);
+    const response = await fetch("http://localhost:8080/api/login", {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            'userName': username,
+            'password': password
+        })
+    }).then(data => {
+        return data.json();
+    }).catch(err => console.error(err))
+    console.log(response);
 }
+loginForm.addEventListener('submit', login);
+});
+
+// function login(e) {
+//     e.preventDefault();
+//     console.log("loged in")
+//     // Get input values
+//     const username = document.getElementById("username").value;
+//     const password = document.getElementById("password").value;
+
+//     // You can add your login logic here (e.g., validate credentials, make API calls, etc.)
+//     // For demonstration purposes, let's just display an alert message:
+//     if (username && password) {
+//         alert("Login successful!");
+//     } else {
+//         alert("Please enter valid credentials.");
+//     }
+// }
