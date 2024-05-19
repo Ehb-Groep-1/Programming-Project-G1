9773a8dca6e553bbabb1c5d3c55905ab24254ead
Ibrahim_Bousba <ibrahim.bousba@gmail.com>
2024-05-19 17:39:15 +0200

    Test (#17)
    * added service and controller
    
    ---------
    
    Co-authored-by: IbrahimBousba <hdydanb97@gmail.com>

--- rental/src/main/java/com/medialab/rental/repository/UserRepository.java
+++ rental/src/main/java/com/medialab/rental/repository/UserRepository.java
@@ -4,12 +4,14 @@ import com.medialab.rental.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.time.LocalDate;
 import java.util.List;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
-    User findByUsername(String username);
+    User findUserById(int id);
+
+    User findUserByUsername(String username);
+
     User findByEmail(String email);
 
 //    List<User> findAll();
