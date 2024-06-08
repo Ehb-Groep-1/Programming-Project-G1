45db132d3b347a965873ea96be32504af1572702
Harold Brinkhof <harold.brinkhof@student.ehb.be>
2024-06-08 15:44:52 +0200

    user notification + session service

--- rental/src/main/java/com/medialab/rental/repository/NotificationRepository.java
+++ rental/src/main/java/com/medialab/rental/repository/NotificationRepository.java
@@ -1,7 +1,9 @@
 package com.medialab.rental.repository;
 
 import com.medialab.rental.Notification;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import java.time.LocalDateTime;
 import java.util.List;
@@ -10,6 +12,9 @@ import java.util.List;
 public interface NotificationRepository extends JpaRepository<Notification, Integer> {
 
     Notification findByUserId(int userID);
+
+    @Query("SELECT notification FROM Notification notification WHERE notification.user.id = :userId")
+    List<Notification> findAllByUserId(int userId);
 
     Notification findByDateSent(LocalDateTime dateSent);
 
